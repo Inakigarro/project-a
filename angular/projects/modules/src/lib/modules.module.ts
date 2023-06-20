d1b7b0814df01a140f4da0a32f995b104ba75e7f
inakigarro <inakigarro@gmail.com>
2023-06-20 19:42:15 -0300

    initial module library commit

---
+++ angular/projects/modules/src/lib/modules.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { ModulesComponent } from './modules.component';
+
+
+
+@NgModule({
+  declarations: [
+    ModulesComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    ModulesComponent
+  ]
+})
+export class ModulesModule { }
---
+++ angular/projects/modules/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of modules
+ */
+
+export * from './lib/modules.service';
+export * from './lib/modules.component';
+export * from './lib/modules.module';
